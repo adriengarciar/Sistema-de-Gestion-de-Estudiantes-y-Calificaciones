789b07b1eb8a19679afd4762e318189ea848f6e7
Xpro1212 <mafh300@gmail.com>
2025-11-28 18:49:53 -0400

    Refactor validation functions and streamline student registration process

--- py/registro.py
+++ py/registro.py
@@ -1,6 +1,7 @@
 def registrar_alumnos():
     
-    def validar_nombre(nombre):
+
+ def validar_nombre(nombre):
         if nombre.strip() == '':
             print('Error, por favor intente colocar algo')
             return False
@@ -11,14 +12,14 @@ def registrar_alumnos():
 
         
         elif not nombre.isalpha():
-                print('error, por favor intente sin numeros')
-                return False
+            print('error, por favor intente sin numeros')
+            return False
             
    
         return True
         
         
-    def validar_apellido(apellido):
+ def validar_apellido(apellido):
         if apellido.strip() == '':
             print('Error, por favor intente colocar algo')
             return False
@@ -35,53 +36,25 @@ def registrar_alumnos():
        
         return True
             
-    def validar_nota(nota):
-        
-        try:
-            acumulado = float(nota)
-        except ValueError:
-            print('Ingrese una entrada valida.(Un acumulado decimal)')
-            return False
-            
-        if acumulado < 0.0 or acumulado > 100.0:
-            print('Esta nota excede los rangos aceptables')
-            return False
-      
-
-        return True
-            
-    while True:
+ while True:
       
         nombre = input('Ingrese el nombre del estudiante: ')
         if validar_nombre(nombre):
             nombre = nombre.capitalize()
             break
-    while True:
+ while True:
         
         apellido = input('Ingrese el apellido del estudiante: ')
         if validar_apellido(apellido):
-            apellido = apellido.capitalize()
-            break
-        
-    while True:
-        
-        nota = input('Ingrese la nota acumulada del estudiante: ')
-        nota_valida = validar_nota(nota)
-        if nota_valida:
-            nota = nota_valida
-            break
-      
-    return nombre, apellido, nota
+         apellido = apellido.capitalize()
+         break  
+ return nombre, apellido
             
-            
-                    
-nombre, apellido, nota = registrar_alumnos()
-
+nombre, apellido = registrar_alumnos()
 
 alumno = {
         'Nombre': nombre,
         'Apellido': apellido,
-        'Nota': nota,
-        }
+        
+}
 
- return alumno
