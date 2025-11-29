51062b8b3b079a991168a31743bf337ccde2aa8f
Xpro1212 <mafh300@gmail.com>
2025-11-28 22:01:11 -0400

    Nueva estructura de funcion de registro de alumnos

--- py/registro.py
+++ py/registro.py
@@ -1,60 +1,43 @@
 def registrar_alumnos():
-    
-
- def validar_nombre(nombre):
+    def validar_nombre(nombre):
         if nombre.strip() == '':
             print('Error, por favor intente colocar algo')
             return False
-            
         elif " " in nombre.strip():
             print("Solo coloque un nombre, sin espacios.")
             return False
-
-        
         elif not nombre.isalpha():
             print('error, por favor intente sin numeros')
             return False
-            
-   
         return True
-        
-        
- def validar_apellido(apellido):
+
+    def validar_apellido(apellido):
         if apellido.strip() == '':
             print('Error, por favor intente colocar algo')
             return False
-            
-        elif " " in nombre.strip():
+        elif " " in apellido.strip():
             print("Solo coloque un apellido, sin espacios.")
             return False
-
-        
         elif not apellido.isalpha():
-                print('error, por favor intente de nuevo')
-                return False
-            
-       
+            print('error, por favor intente de nuevo')
+            return False
         return True
-            
- while True:
-      
+
+    while True:
         nombre = input('Ingrese el nombre del estudiante: ')
         if validar_nombre(nombre):
             nombre = nombre.capitalize()
             break
- while True:
-        
+
+    while True:
         apellido = input('Ingrese el apellido del estudiante: ')
         if validar_apellido(apellido):
-         apellido = apellido.capitalize()
-         break  
- return nombre, apellido
-            
-nombre, apellido = registrar_alumnos()
+            apellido = apellido.capitalize()
+            break
 
-alumno = {
-        'Nombre': nombre,
-        'Apellido': apellido,
-        
-}
+    return nombre, apellido
 
+
+if __name__ == "__main__":
+    nombre, apellido = registrar_alumnos()
+    print(f'Nombre: {nombre}, Apellido: {apellido}')
